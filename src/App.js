3fcbf8c620d9430d9e44ee1945d0a4fa15fa15a4
yeedme <1812503349@qq.com>
2024-08-28 16:16:55 +0800

    feat: AD

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
-import Threeinital from "./components/three/Threeinital";
+// import Threeinital from "./components/three/Threeinital";
+ import Inital from "./components/one/Inital";
 export default function App() {
 
   return (
     <>
-      {/* <Inital/> */}
-      <Threeinital/>
+      <Inital/>
+      {/* <Threeinital/> */}
       
     </>
   );
+
 }
